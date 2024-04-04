f7f63f0f36cde139853e35b868ff29ad925ed8aa
Kate Kanoplich <kanoplickata@gmail.com>
2024-04-04 14:24:25 +0300

    first commit

---
+++ task1/task1/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        FileSystemEmulator emulator = new FileSystemEmulator();
+        emulator.run();
+    }
+}
